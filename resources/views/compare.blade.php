fd1859d17361e6e4082aa4cac534eebedbeb5342
shazolkh <shazol.kh@gmail.com>
2020-12-11 02:23:44 +0600

    Ads test Index page

--- resources/views/compare.blade.php
+++ resources/views/compare.blade.php
@@ -118,22 +118,22 @@
                         @endforeach
                     </select>
                     <input type="submit" class="botarticle-more" value="Compare">
+                    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                    <!-- bikestall_horizonal -->
+                    <ins class="adsbygoogle"
+                         style="display:block"
+                         data-ad-client="ca-pub-1900204270155608"
+                         data-ad-slot="3176536605"
+                         data-ad-format="auto"
+                         data-full-width-responsive="true"></ins>
+                    <script>
+                        (adsbygoogle = window.adsbygoogle || []).push({});
+                    </script>
                 </div>
             </form>
             <a href="#" class="botarticle-img">
                 <img src="{{asset('public/frontend/img/article1.jpg')}}" alt="">
             </a>
-            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-            <!-- bikestall_horizonal -->
-            <ins class="adsbygoogle"
-                 style="display:block"
-                 data-ad-client="ca-pub-1900204270155608"
-                 data-ad-slot="3176536605"
-                 data-ad-format="auto"
-                 data-full-width-responsive="true"></ins>
-            <script>
-                (adsbygoogle = window.adsbygoogle || []).push({});
-            </script>
         </div>
     </div>
 </main>
