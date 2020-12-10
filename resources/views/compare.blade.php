d5eb236d76e3cf3c7575a148a3843b3221507249
shazolkh <shazol.kh@gmail.com>
2020-12-11 02:17:51 +0600

    ADs-- Compare page

--- resources/views/compare.blade.php
+++ resources/views/compare.blade.php
@@ -120,6 +120,18 @@
                     <input type="submit" class="botarticle-more" value="Compare">
                 </div>
             </form>
+            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+            <!-- bikestall_horizonal -->
+            <ins class="adsbygoogle"
+                 style="display:block"
+                 data-ad-client="ca-pub-1900204270155608"
+                 data-ad-slot="3176536605"
+                 data-ad-format="auto"
+                 data-full-width-responsive="true"></ins>
+            <script>
+                (adsbygoogle = window.adsbygoogle || []).push({});
+            </script>
+
             <a href="#" class="botarticle-img">
                 <img src="{{asset('public/frontend/img/article1.jpg')}}" alt="">
             </a>
