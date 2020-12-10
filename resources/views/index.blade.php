58d0637c309e6c86bb2bc43790351c2fcbe29794
shazolkh <shazol.kh@gmail.com>
2020-12-10 23:34:42 +0600

    Ads test Index page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -168,17 +168,24 @@
 		<span class="frontsearch-line2"></span>
 	</div>
 </div>
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<!-- bikestall responsive -->
-<ins class="adsbygoogle"
-     style="display:block"
-     data-ad-client="ca-pub-1900204270155608"
-     data-ad-slot="5000779542"
-     data-ad-format="auto"
-     data-full-width-responsive="true"></ins>
-<script>
-    (adsbygoogle = window.adsbygoogle || []).push({});
-</script>
+
+<div class="populars-wrap">
+    <div class="cont populars">
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        <!-- bikestall responsive -->
+        <ins class="adsbygoogle"
+             style="display:block"
+             data-ad-client="ca-pub-1900204270155608"
+             data-ad-slot="5000779542"
+             data-ad-format="auto"
+             data-full-width-responsive="true"></ins>
+        <script>
+            (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
+        <span class="popular-line1"></span>
+        <span class="popular-line2"></span>
+    </div>
+</div>
 
 
 <!-- Popular Items -->
